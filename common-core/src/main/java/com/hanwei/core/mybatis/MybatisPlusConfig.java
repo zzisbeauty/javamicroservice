ea7e56dd05f8cd94933d9a4d9c0105da1744fce6
zzisbeauty <swhardbewell@qq.com>
2025-12-31 08:43:40 +0800

    init-2

--- common-core/src/main/java/com/hanwei/core/mybatis/MybatisPlusConfig.java
+++ common-core/src/main/java/com/hanwei/core/mybatis/MybatisPlusConfig.java
@@ -15,19 +15,14 @@ import org.springframework.context.annotation.Configuration;
  * @Since version-1.0
  */
 @Configuration
-@MapperScan(value={"com.hanwei.**.mapper*"})
+@MapperScan(value={"com.hanwei.**.mapper*"})  // 扫描所有 mapper 接口
 public class MybatisPlusConfig {
-
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
         // 添加乐观锁拦截器
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
-
         return interceptor;
     }
-
-
 }
